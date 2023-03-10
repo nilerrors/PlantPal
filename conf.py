759afb5fdd41c16e7e41657affb53271eabc08ca
nilerrors <enayat.sabawoon@outlook.com>
2023-03-10 21:06:39 +0100

    Add conf.py script for project configuration

---
+++ conf.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import socket
+import secrets
+
+
+if __name__ == '__main__':
+    CLIENT_PATH = os.curdir + '/client'
+    ESP32_PATH = os.curdir + '/plantpal'
+    SERVER_PATH = os.curdir + '/server'
+    IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+
+    with open(CLIENT_PATH + '\.env', 'w') as f:
+        f.write(f'VITE_API_BASE_URL="http://{IP_ADDRESS}:8000"\n')
+
+    os.system(f'cd {CLIENT_PATH} && yarn')
+    print('cd client && yarn start')
+
+    with open(SERVER_PATH + '\.env', 'w') as f:
+        text = f'AUTHJWT_SECRET_KEY="{secrets.token_hex()}"\n\n'
+        text += 'MAIL_USERNAME="enayat.sabawoon@outlook.com"\n'
+        text += 'MAIL_PASSWORD=""\n'
+        text += 'MAIL_FROM="enayat.sabawoon@outlook.com"\n'
+        text += 'MAIL_PORT=587\n'
+        text += 'MAIL_SERVER=smtp.office365.com\n'
+        text += 'MAIL_FROM_NAME="PlantPal"\n\n'
+        text += 'DATABASE_URL="postgresql://postgres:postgres@localhost:5432/management"\n'
+        f.write(text)
+
+    os.system(f'cd {SERVER_PATH} && py -m venv venv && .\\venv\\Script\\Activate.ps1 && pip install -r deps.txt')
