a89cd66237d5a52098fff585b8f590527c61f681
nilerrors <enayat.sabawoon@outlook.com>
2023-05-05 23:22:37 +0200

    remove app

--- app/constants/Api.ts
+++
@@ -1,1 +0,0 @@
-export const API_URL = "http://192.168.129.7:8000";
--- app/constants/Wifi.ts
+++
@@ -1,6 +0,0 @@
-export const WIFI = {
-  ssid: "PlantPalConfig",
-  pass: "plantpal",
-  // ssid: "Proximus-Home-038934",
-  // pass: "ubs67anbm4nckf3f",
-} as const;
